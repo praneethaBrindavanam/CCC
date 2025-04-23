54f0e50605ade861e64c15d09a6b23a95e5c5579
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-04-23 11:56:03 +0530

    Create random.js

---
+++ mis files/random.js
@@ -0,0 +1,8 @@
+var random=4;
+let randomInt= Math.floor (Math.random()*10);
+if(random==randomInt){
+    console.log("true",randomInt);
+}
+else 
+console.log("false",random,randomInt);
+//console.log(random==randomInt);
