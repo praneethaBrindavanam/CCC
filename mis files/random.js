3ca021e360870cd48430ea872315e3393a23249d
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-04-23 13:29:07 +0530

    Update random.js

--- mis files/random.js
+++ mis files/random.js
@@ -1,3 +1,4 @@
+//random numbers to generate and compare
 var random=4;
 let randomInt= Math.floor (Math.random()*10);
 if(random==randomInt){
@@ -6,3 +7,11 @@ if(random==randomInt){
 else 
 console.log("false",random,randomInt);
 //console.log(random==randomInt);
+---------------------------------------------
+
+//map method
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+const res=numbers.map((num)=>{
+    return num+20
+})
+console.log(res);
